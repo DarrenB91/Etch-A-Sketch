c29b8c95c8def05f4220a29b7640c4ac0ee15a35
DarrenB91 <dbowlesbusiness@gmail.com>
2023-04-17 16:28:44 +0100

    Yo

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
+
+//Selectors 
 const grid =document.querySelector(".grid");
 const grid_size = document.querySelector(".size-value");
+const size_slider = document.querySelector("#sizeSlider");
+const size_display = document.querySelector("#sizeValue");
+
+//Modes 
+let currentMode = 'color';
+let currentSize = 16;
 
 
-function changeColor() { 
-  console.log("hey");
+
+//Size 
+size_slider.addEventListener("input", function() {
+  currentSize = size_slider.value;
+  makeGrid(currentSize);
+  size_display.innerHTML = `${currentSize} x ${currentSize}`;
+});
+
+//Function that Changes the Color To Black 
+function changeColor(e) { 
+    e.target.style.backgroundColor = 'black';
 };
 
 
+//Function that Makes Grid and Allows Hover 
+function makeGrid(size) {
+  grid.innerHTML = "";
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-function makeGrid(size) { 
-  for (i = 0; i < size; i++) { 
+  for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement('div');
     gridElement.classList.add('square');
-    gridElement.addEventListener('mouseover', changeColor)
-    gridElement.addEventListener('mousedown', changeColor)
+    gridElement.addEventListener('mouseover', changeColor);
     grid.appendChild(gridElement);
-    }
-};
-
-makeGrid(16);
-console.log("Script loaded.");
+  }
+}
+makeGrid(currentSize);
